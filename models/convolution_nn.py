faa83387955a1d311d9c35cf6fca7b638f019ecc
qlai <qlai@mit.edu>
2017-01-28 19:25:57 +0000

    minor fix

--- models/convolution_nn.py
+++ models/convolution_nn.py
@@ -9,72 +9,71 @@ import tensorflow as tf
 
 from utils import *
 
-
-class ConvolutionNN():
-	def __init__(self, input_dim, output_dim, hidden_layers, layer_types, activations, learning_rate, dropout = False, costfunc = cross_entropy): 
-		''' multilayer perceptron class for simple models
-		if dropout == True: feed must include drop out probability named 'keep_prob', else feed includes 'input_data', 'target_data'
-	    hidden_layers and activations are lists of layer dimensions (int) and strings 
-	    note train_step'''
-	    # TODO: add different cost functions
+from est_base import EstBase
 
 
-		self.input_dim = input_dim
-		self.output_dim = output_dim
-		self.hidden_layers = hidden_layers
-		self.activations = activations
-		self.costfunc = costfunc
-		self.learning_rate = learning_rate
-		self.dropout = dropout
+class MultiLayerPerceptron(EstBase):
+    def __init__(self, input_dim, output_dim, hidden_types, hidden_dims, activations, learning_rate, dropout = False, costfunc = cross_entropy):
+        ''' multilayer perceptron class for simple models
+        if dropout == True: feed must include drop out probability named 'keep_prob', else feed includes 'input_data', 'target_data'
+        hidden_dims and activations are lists of layer dimensions (int) and strings 
+        note train_step'''
+        # TODO: add different cost functions
+
+        super(MultiLayerPerceptron, self).__init__(input_dim, output_dim, \
+                                                   costfunc, learning_rate)
+
+        self.hidden_dims = hidden_dims
+        self.activations = activations
+        self.dropout = dropout
+
+        #define placeholders for data
+        with tf.name_scope('input'):
+            self.input_data = tf.placeholder(tf.float32, [None, self.input_dim], name='input_data')
+            self.target_data = tf.placeholder(tf.float32, [None, self.output_dim], name='target_data')
+
+        #define neural network
+        self.num_layers = len(hidden_dims)
+
+        self.hidden = []
+
+        for i in range(self.num_layers):
+            if i == 0:
+                self.hidden.append(layer(self.input_data, self.input_dim, self.hidden_dims[i], \
+                                'hidden_{}'.format(i+1), self.activations[i]))
+
+            # hidden_next = layer(self.hidden_previous, hidden_dims[i-1], hidden_dims[i], 'hidden_#number', activations[i])
+            else:
+                self.hidden.append(layer(self.hidden[i-1], self.hidden_dims[i-1], self.hidden_dims[i], \
+                                'hidden_{}'.format(i+1), self.activations[i]))
+
+        if dropout:
+            self.keep_prob = tf.placeholder(tf.float32)
+            tf.summary.scalar('dropout_keep_probability', self.keep_prob)
+            dropped = tf.nn.dropout(self.hidden[-1], self.keep_prob)
+            self.output = layer(dropped, self.hidden_dims[-1], self.output_dim, 'output_layer')
+
+        else:
+            self.output = layer(self.hidden[-1], self.hidden_dims[-1], self.output_dim, 'output_layer')
+
+        #def cost with function, real y and output y 
+        with tf.name_scope('cost'):
+            self.diff = self.costfunc(self.target_data, self.output)
+            with tf.name_scope('total'):
+                self.cost= tf.reduce_mean(self.diff)
+                tf.summary.scalar('cost', self.cost)
+
+        with tf.name_scope('train'):
+            self.train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
 
 
-		#define placeholders for data
-		with tf.name_scope('input'):
-			self.input_data = tf.placeholder(tf.float32, [None, input_dim], name='input_data')
-		    self.target_data = tf.placeholder(tf.float32, [None, output_dim], name='target_data')
+        with tf.name_scope('accuracy'):
+            with tf.name_scope('correct_prediction'):
+                correct_prediction = tf.equal(tf.argmax(self.output, 1), tf.argmax(self.target_data, 1))
+            with tf.name_scope('accuracy'):
+                self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy', self.accuracy)
+        self.merged = tf.summary.merge_all()
 
-		#define neural network
-		self.num_layers = len(hidden_layers)
-
-		self.hidden = []
-
-		for i in range(self.num_layers):
-			if i == 0:
-				self.hidden.append(layer(self.input_data, input_dim, hidden_layers[i], \
-								'hidden_{}'.format(i+1), activations[i]))
-
-			# hidden_next = layer(self.hidden_previous, hidden_layers[i-1], hidden_layers[i], 'hidden_#number', activations[i])
-			else:
-				self.hidden.append(layer(hidden[i-1], hidden_layers[i-1], hidden_layers[i], \
-								'hidden_{}'.format(i+1), activations[i]))
-
-		if dropout:
-		    self.keep_prob = tf.placeholder(tf.float32)
-		    tf.summary.scalar('dropout_keep_probability', self.keep_prob)
-		    dropped = tf.nn.dropout(self.hidden[-1], self.keep_prob)
-		    self.output = layer(dropped, self.hidden_layers[-1], self.output_dim, 'output_layer')
-
-		else:
-			self.output = layer(self.hidden[-1], self.hidden_layers[-1], self.output_dim, 'output_layer')
-
-		#def cost with function, real y and output y 
-		with tf.name_scope('cost'):
-			self.diff = self.costfunc(self.target_data, self.output)
-			with tf.name_scope('total'):
-				self.cost= tf.reduce_mean(self.diff)
-			    tf.summary.scalar('cost', self.cost)
-
-		with tf.name_scope('train'):
-			self.train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
-
-
-		with tf.name_scope('accuracy'):
-		    with tf.name_scope('correct_prediction'):
-			    correct_prediction = tf.equal(tf.argmax(self.output, 1), tf.argmax(self.target, 1))
-		    with tf.name_scope('accuracy'):
-		    	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-		tf.summary.scalar('accuracy', accuracy)
-  		self.merged = tf.summary.merge_all()
-
-		#define training operation
-		
+        #define training operation
+        
