f4462db4215b6bff0a05001febe35017bd1602ff
RenqiaoZhang <andy17@mit.edu>
2017-01-28 17:43:27 +0000

    updated test()

--- models/train.py
+++ models/train.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
 
-dataset = input_data.read_data_sets()
-
-
 
 def train(model, dataset, NUM_ITERS, BATCH_SIZE, LOG_DIR, KEEP_PROB=1.):
     '''
-
+    Training a model
+    :param model: model object defined by Qiuyin
+    :param dataset: dataset object,
+            i.e. dataset=input_data.read_data_sets(dir)
+                same as when you read the built-in mnist
+    :param NUM_ITERS:   int
+    :param BATCH_SIZE:  int
+    :param LOG_DIR: the directory to log files
+    :param KEEP_PROB: dropout probability
+    :return:
     '''
     sess = tf.Session()
     tf.initialize_all_variables().run()
@@ -16,20 +22,31 @@ def train(model, dataset, NUM_ITERS, BATCH_SIZE, LOG_DIR, KEEP_PROB=1.):
     for i in range(NUM_ITERS):
         xs, ys = dataset.train.next_batch(BATCH_SIZE)
 
-        feeded = {model.input_data:xs, model.target_data:ys}
+        fed = {model.input_data:xs, model.target_data:ys}
         if model.dropout:
-            feeded[model.keep_prob]=KEEP_PROB
+            fed[model.keep_prob]=KEEP_PROB
 
-        sess.run( model.train_step, feed_dict = feeded )
+        sess.run( model.train_step, feed_dict = fed )
 
         if i % 100 == 99:
-            summary, acc = sess.run([model.merged,model.accuracy], feed_dict=feeded)
+            summary, acc = sess.run([model.merged,model.accuracy], feed_dict=fed)
             train_writer.add_summary(summary,i)
             print('Accuracy at step %s: %s' % (acc, i))
     train_writer.close()
 
 
-def testing(model, dataset ):
-    test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/test')
+def testing(model, dataset, LOG_DIR, TEST_ID =0 ):
+    '''
+    :param model: Qiuyin's model
+    :param dataset:
+    :param LOG_DIR:
+    :param TEST_ID:
+    :return:
+    '''
+    sess = tf.Session()
+    test_writer = tf.summary.FileWriter(LOG_DIR + '/test')
     test_x, test_labels = dataset.test.images, dataset.test.labels
+    fed = {model.input_data:test_x, model.target_data:test_labels}
+    summary,acc = sess.run([model.merged, model.accuracy], feed_dict=fed)
+    test_writer.add_summary(summary,TEST_ID)
 
