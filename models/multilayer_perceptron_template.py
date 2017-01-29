9cfeea6a9a797607f9cb8ce10401131a7a94fdaf
zhoudada <zhoudada123@hotmail.com>
2017-01-29 03:32:02 +0000

    Added multilayer_perceptron_template and its output. Added json parser in server.

---
+++ models/multilayer_perceptron_template.py
@@ -0,0 +1,95 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+
+import tensorflow as tf
+
+import utils
+
+from est_base import EstBase
+
+
+class MultiLayerPerceptron(EstBase):
+    def __init__(self):
+        ''' multilayer perceptron class for simple models
+        if dropout == True: feed must include drop out probability named 'keep_prob', else feed includes 'input_data', 'target_data'
+        hidden_layers and activations are lists of layer dimensions (int) and strings
+        note train_step'''
+        # TODO: add different cost functions
+
+        # costfunc default to be cross_entropy
+        super(MultiLayerPerceptron, self).__init__({{ input_dim }}, {{ output_dim }}, \
+                                                   {{ costfunc }}, {{ learning_rate }}, \
+                                                   {{ optimizer }})
+
+        self.hidden_dims = {{ hidden_layers_dims }}
+        self.activations = {{ activations }}
+        self.dropout = {{ dropout }}
+
+        #define placeholders for data
+        with tf.name_scope('input'):
+            self.input_data = tf.placeholder(tf.float32, [None, self.input_dim], name='input_data')
+            self.target_data = tf.placeholder(tf.float32, [None, self.output_dim], name='target_data')
+
+        #define neural network
+        self.num_layers = len(self.hidden_dims)
+
+        {% for i in range(layer_num) %} {% if i == 0 %}
+        hidden_layer_{{ i + 1 }} = utils.perceptron(self.input_data, self.input_dim, self.hidden_dims[{{i}}], \
+                                'hidden_{}'.format({{ i }}), self.activations[{{ i }}])
+        {% elif i == layer_num - 1 %}
+        hidden_layer_last = utils.perceptron(hidden_layer_{{ i }}, self.hidden_dims[{{i - 1}}], \
+                                               self.hidden_dims[{{i}}], 'hidden_{}'.format({{i + 1}}), self.activations[{{i}}])
+        {% else %}
+        hidden_layer_{{ i + 1 }} = utils.perceptron(hidden_layer_{{ i }}, self.hidden_dims[{{i - 1}}], \
+                                                      self.hidden_dims[{{i}}], 'hidden_{}'.format({{i + 1}}), self.activations[{{i}}])
+        {% endif %} {% endfor %}
+
+        # for i in range(self.num_layers):
+        # 	if i == 0:
+        # 		self.hidden.append(layer(self.input_data, self.input_dim, self.hidden_layers_dims[i], \
+        # 						'hidden_{}'.format(i+1), self.activations[i]))
+        #
+        # 	# hidden_next = layer(self.hidden_previous, hidden_layers[i-1], hidden_layers[i], 'hidden_#number', activations[i])
+        # 	else:
+        # 		self.hidden.append(layer(hidden[i-1], self.hidden_layers_dims[i - 1], self.hidden_layers_dims[i], \
+        # 						'hidden_{}'.format(i+1), self.activations[i]))
+
+        if self.dropout:
+            self.keep_prob = tf.placeholder(tf.float32)
+            tf.summary.scalar('dropout_keep_probability', self.keep_prob)
+            dropped = tf.nn.dropout(hidden_layer_last, self.keep_prob)
+            self.output = utils.perceptron(dropped, self.hidden_dims[-1], self.output_dim, 'output_layer')
+
+        else:
+            self.output = utils.perceptron(hidden_layer_last, self.hidden_dims[-1], self.output_dim, 'output_layer')
+
+        #def cost with function, real y and output y
+        with tf.name_scope('cost'):
+            self.diff = self.costfunc(self.target_data, self.output)
+            with tf.name_scope('total'):
+                self.cost = tf.reduce_mean(self.diff)
+                tf.summary.scalar('cost', self.cost)
+
+        with tf.name_scope('train'):
+            # self.train_step = utils.get_optimizer(self.learning_rate, self.cost, self.optimizer)
+            if self.optimizer == 'ADAM':
+                self.train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(self.cost)
+            elif self.optimizer == 'GD':
+                self.train_step = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.cost)
+            else:# default choice of optimizer to be GD
+                self.train_step = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.cost)
+
+
+        with tf.name_scope('accuracy'):
+            with tf.name_scope('correct_prediction'):
+                correct_prediction = tf.equal(tf.argmax(self.output, 1), tf.argmax(self.target, 1))
+            with tf.name_scope('accuracy'):
+                accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy', accuracy)
+        self.merged = tf.summary.merge_all()
+
+        #define training operation
--- server/flowchart_parser.py
+++ server/flowchart_parser.py
@@ -1,4 +1,21 @@
 import jinja2
+import json
+
+class ModelData(object):
+    def __init__(self, input_dim, output_dim, activations, learning_rate, optimizer='\'ADAM\''):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.activations = activations
+        self.learning_rate = learning_rate
+        self.optimizer = optimizer
+
+class MultilayerPerceptronData(ModelData):
+    def __init__(self, input_dim, output_dim, hidden_layers_dims, activations, \
+                     learning_rate, dropout = 'False', costfunc = 'utils.cross_entropy'):
+        super(MultilayerPerceptronData, self).__init__(input_dim, output_dim, activations, learning_rate)
+        self.hidden_layers_dims = hidden_layers_dims
+        self.dropout = dropout
+        self.costfunc = costfunc
 
 
 class FlowchartParser:
@@ -7,7 +24,139 @@ class FlowchartParser:
             loader=jinja2.FileSystemLoader('../models')
         )
 
+    def _try_parse_int(self, s):
+        ret = None
+        # try:
+        #     ret = int(s)
+        #
+        # except ValueError:
+        #     pass
+        ret = int(s)
 
-    def parse_and_output(self, josn_str, filename):
-        template = self.env.get_template('model_template.py')
-        print(template.render())
+        return ret
+
+    def _parse_para_string(self, para_string):
+        para_list = para_string.split('\n')
+        paras = [para.split(':')[1].lstrip() for para in para_list]
+        return paras
+
+    def _parse_input_node(self, input_node):
+        input_para = input_node['para']
+        paras = self._parse_para_string(input_para)
+        input_dim = int(paras[0])
+        learning_rate = float(paras[1])
+        return input_dim, learning_rate
+
+    def _parse_nodes(self, head_key, key_to_node, key_to_next, model_type):
+        node = key_to_node[head_key]
+        input_dim, learning_rate = self._parse_input_node(node)
+        model_data = None
+        layer_dims = []
+        activations = []
+        output_dim = None
+        key_num = len(key_to_node)
+
+        node = key_to_next[head_key]
+
+        while node['key'] in key_to_node:
+            if node['text'].lower() == 'output':
+                paras = self._parse_para_string(node['para'])
+                output_dim = self._try_parse_int(paras[0])
+                break
+
+            if model_type == 0:
+                # Multi-layer perceptron model
+                para_string = node['para']
+                paras = self._parse_para_string(para_string)
+                layer_dim = int(paras[0])
+                activation = paras[1].lower()
+                multi_layer_num = self._try_parse_int(paras[2])
+                if multi_layer_num:
+                    layer_dims.extend([layer_dim] * multi_layer_num)
+                    activations.extend([activation] * multi_layer_num)
+                else:
+                    layer_dims.append(layer_dim)
+                    activations.append(activation)
+
+            node = key_to_next[node['key']]
+
+
+        if model_type == 0:
+            model_data = MultilayerPerceptronData(input_dim, output_dim, layer_dims, activations, \
+                    learning_rate)
+        elif model_type == 1:
+            pass
+
+        elif model_type == 2:
+            pass
+
+        return model_data
+
+    def _parse_graph_description(self, graph_description_dict):
+        nodes = graph_description_dict['nodeDataArray']
+        key_to_node = {}
+        head_key = None
+        for node in nodes:
+            key = node['key']
+            key_to_node[key] = node
+            if node['text'].lower() == 'input':
+                head_key = key
+
+        key_to_next = {}
+        for link in graph_description_dict['linkDataArray']:
+            from_key = link['from']
+            to_key = link['to']
+            key_to_next[from_key] = key_to_node[to_key]
+
+        # Parse input dimension
+        input_node = key_to_node[head_key]
+
+        # Find the type of model
+        first_layer_node = key_to_next[head_key]
+        model_type = None
+        first_layer_name = first_layer_node['text']
+        if first_layer_name.lower() == 'perceptron layer':
+            model_type = 0
+
+        elif first_layer_name.lower() == 'convolution layer':
+            model_type = 1
+
+        elif first_layer_name.lower() == 'recurrent layer':
+            model_type = 2
+
+        model_data = self._parse_nodes(head_key, key_to_node, key_to_next, model_type)
+
+        return model_data
+
+
+    def _parse_multilayer_perceptron_and_output(self, model_data, filename):
+        # input_dim = '8'
+        # output_dim = '2'
+        # hidden_layers_dims_list = [4, 5, 6, 7, 8]
+        # activations = "['relu', 'relu', 'softplus', 'sigmoid', 'tanh']"
+        # learning_rate = '0.563'
+        # dropout = 'True'
+
+        template_filename = 'multilayer_perceptron_template.py'
+        template = self.env.get_template(template_filename)
+        # hidden_layers_dims_json_str = json.dumps(model_data.hidden_layers_dims)
+        hidden_layers_dims = model_data.hidden_layers_dims
+        layer_num = len(model_data.hidden_layers_dims)
+        render_output = template.render(input_dim=model_data.input_dim, output_dim=model_data.output_dim,
+                                        hidden_layers_dims=hidden_layers_dims, layer_num=layer_num, \
+                                        activations=model_data.activations, learning_rate=model_data.learning_rate, \
+                                        dropout=model_data.dropout, costfunc=model_data.costfunc, \
+                                        optimizer=model_data.optimizer)
+        with open('../models/' + filename, 'w') as f:
+            f.write(render_output)
+
+    def parse_and_output(self, json_str):
+        graph_description_dict = json.loads(json_str)
+        model_data = self._parse_graph_description(graph_description_dict)
+        if not model_data:
+            print('Error in parsing')
+        else:
+            if isinstance(model_data, MultilayerPerceptronData):
+                output_filename = 'multilayer_perceptron_output.py'
+                self._parse_multilayer_perceptron_and_output(model_data, output_filename)
+
