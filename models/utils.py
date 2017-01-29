eb0cca71d32d71903950ae5743dde31f6ed3807b
qlai <qlai@mit.edu>
2017-01-29 00:04:32 +0000

    cnn

--- models/utils.py
+++ models/utils.py
@@ -53,14 +53,17 @@ def perceptron(input, input_dim, output_dim, layer_name, act = None, summaries =
 
     return output
 
+
 def flatten(input, input_channels, output_dim, num_prev_cnn, layer_name, act = None, summaries = True):
     INPUT_DIM = 28
     cnn_out_width = INPUT_DIM
     for i in range(num_prev_cnn):
         cnn_out_width = math.ceil(cnn_out_width/2.)
     ## cnn_out_width
-    flattened_input = tf.reshape(input,  [(cnn_out_width**2)*input_channels, output_dim])
-    return perceptron(flattened_input, )
+    flattened_dim =(cnn_out_width**2)*input_channels, output_dim
+    flattened_input = tf.reshape(input,  [flattened_dim])
+    return perceptron(flattened_input, flattened_dim, output_dim, layer_name, act = act, summaries = summaries)
+
 
 def convolution_layer(input, input_dim, output_dim, patchsize, layer_name, act = 'relu', summaries = True):
     with tf.name_scope(layer_name):
